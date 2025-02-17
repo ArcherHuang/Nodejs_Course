4ba3b2979546d6edebe48fda866f476fae8191d2
ArcherHuang <mmosconii@gmail.com>
2025-02-17 15:52:12 +0800

    Update

--- Course/6_ifelse.js
+++ Course/6_ifelse.js
@@ -2,6 +2,7 @@
 //      node 6_ifelse.js 
 // Output:
 //      非常好
+//      非常非常好
 
 const score = 90;
 
@@ -12,3 +13,10 @@ if (score >= 90) {
 } else {
     console.log('中等');
 }
+
+const name = 'Archer';
+const score2 = 90;
+
+if (score2 >= 90 && name === 'Archer') {
+    console.log('非常非常好');
+} 
